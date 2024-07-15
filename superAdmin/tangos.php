538a485b3d0370fe147fe2fed75b08322be8041f
tkamado052 <tkamado052@gmail.com>
2024-07-15 11:57:54 +0800

    super admin

---
+++ superAdmin/tangos.php
@@ -0,0 +1,82 @@
+<?php include('includes/header.php'); ?>
+
+<div class="container-fuild py-4">
+    <div class="row">
+        <div class="col-md-12">
+        
+      <div class="row mt-4">
+          <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+            <div class="card">
+              <div class="card-header p-3 pt-2">
+                <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark shadow text-center border-radius-xl mt-n4 position-absolute">
+                <i class="material-icons opacity-10">person</i>
+               </div>
+              <div class="text-end pt-1">
+                  <p class="text-sm mb-0 text-capitalize">Total Senior Citizen</p>
+                    <h4 class="mb-0">281</h4>
+                </div>
+            </div>
+
+             <hr class="dark horizontal my-0">
+            <div class="row">
+              <div class="card-footer p-3">
+              <div class="text-end"> <a class="btn bg-gradient-dark mb-0" href="tangoslist.php">View information</a>
+              </div>
+             </div>
+            </div>
+           </div>
+          </div>
+         
+
+
+
+     <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+     <div class="card ">
+           <div class="card-header p-3 pt-2">
+           <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary shadow text-center border-radius-xl mt-n4 position-absolute">
+           <i class="material-icons opacity-10">person</i>
+            </div>
+            <div class="text-end pt-1">
+           <p class="text-sm mb-0 text-capitalize">Total PWD</p>
+           <h4 class="mb-0">2,300</h4>
+           </div>
+           </div>
+
+           <hr class="dark horizontal my-0">
+           <div class="row">
+              <div class="card-footer p-3">
+              <div class="text-end"> <a class="btn bg-gradient-primary mb-0" href="tanza1list.php">View information</a>
+              </div>
+             </div>
+            </div>
+           </div>
+          </div>
+
+
+      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+           <div class="card">
+           <div class="card-header p-3 pt-2">
+           <div class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
+           <i class="material-icons opacity-10">person</i>
+            </div>
+           <div class="text-end pt-1">
+            <p class="text-sm mb-0 text-capitalize ">Total Solo Parent</p>
+           <h4 class="mb-0 ">34k</h4>
+           </div>
+           </div>
+
+           <hr class="dark horizontal my-0">
+           <div class="row">
+              <div class="card-footer p-3">
+              <div class="text-end"> <a class="btn bg-gradient-success mb-0" href="tanza1list.php">View information</a>
+              </div>
+             </div>
+            </div>
+           </div>
+          </div>
+
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
---
+++ superAdmin/tanza2list.php
@@ -0,0 +1,33 @@
+<?php include('includes/header.php'); ?>
+
+<div class="container-fuild py-4">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4> List </h4>
+                </div>
+                <div class="card-body">
+                    <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ID number</th>
+                            <th>Name</th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>132123</td>
+                            <th>Jefferson Cyrus C. Cavaneyro</th>
+                            
+                        </tr>
+                    </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
